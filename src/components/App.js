ae9a5c7a7b30b1d217c97d1a01a22223e21135cd
+ <jacqueline.trapp18@gmail.com>
2024-11-21 13:07:12 -0500

    lab complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
-// create your App component here
+import {useState, useEffect} from "react";
+
+const App = () => {
+    const API = "https://dog.ceo/api/breeds/image/random";
+    const [appInfo, setAppInfo] = useState("");
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+    
+        fetch(API)
+        .then(res=>res.json())
+        .then(data=> {
+            setAppInfo(<img src={data.message} alt="A Random Dog"></img>);
+            setIsLoaded(true);
+        })
+
+    }, []);
+
+    if (!isLoaded) return <p>Loading...</p>;
+
+    return (appInfo);
+}
+
+export default App;
+
